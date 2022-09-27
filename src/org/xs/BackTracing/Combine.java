2c10897a2a104a8123cde89f6024d2e43f369848
GengYF <2104378862@qq.com>
2022-09-27 17:40:00 +0800

    LeetCode练习——代码随想录 组合问题2

--- src/org/xs/BackTracing/Combine.java
+++ src/org/xs/BackTracing/Combine.java
@@ -14,9 +14,13 @@ import java.util.stream.Collectors;
  * 3. @date: 2022年09月22日 9:44
  */
 public class Combine {
+
+    List<Integer> path = new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
+
     /*77. 组合
-给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
-你可以按 任何顺序 返回答案。*/
+    给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+    你可以按 任何顺序 返回答案。*/
     public List<List<Integer>> combine(int n, int k) {
         int startIndex = 1;
         List<Integer> path = new ArrayList<>();
@@ -90,7 +94,7 @@ public class Combine {
     /*17. 电话号码的字母组合
     给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
     给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。*/
-    //设置全局列表存储最后的结果
+    // 设置全局列表存储最后的结果
     List<String> list = new ArrayList<>();
 
     public List<String> letterCombinations(String digits) {
@@ -105,10 +109,10 @@ public class Combine {
 
     }
 
-    //每次迭代获取一个字符串，所以会设计大量的字符串拼接，所以这里选择更为高效的 StringBuild
+    // 每次迭代获取一个字符串，所以会设计大量的字符串拼接，所以这里选择更为高效的 StringBuild
     StringBuilder temp = new StringBuilder();
 
-    //比如digits如果为"23",num 为0，则str表示2对应的 abc
+    // 比如digits如果为"23",num 为0，则str表示2对应的 abc
     public void backTracking(String digits, String[] numString, int num) {
         //遍历全部一次记录一次得到的字符串
         if (num == digits.length()) {
@@ -131,9 +135,6 @@ public class Combine {
     找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
     candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。
     对于给定的输入，保证和为 target 的不同组合数少于 150 个。*/
-    List<Integer> path = new ArrayList<>();
-    List<List<Integer>> result = new ArrayList<>();
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         backTracing_combinationSum(candidates, target, 0);
         return result;
@@ -167,6 +168,181 @@ public class Combine {
 
     /*40. 组合总和 II
     给定一个候选人编号的集合 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
-    candidates 中的每个数字在每个组合中只能使用 一次 。
+    candidates 中的每个数字在每个组合中只能使用 一次 。（集合中有重复元素）
     注意：解集不能包含重复的组合。 */
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int[] used = new int[candidates.length];
+        Arrays.sort(candidates);
+        backTracing_combinationSum2(candidates, target, used, 0, 0);
+        return result;
+    }
+
+    /**
+     * @description 由于有重复元素，需要有一个标志位来记录树形结构上树层出现相邻两元素相同并且已经查找过的情况，也就是说假如集合中有3个1，在第一层遍历
+     * 时不需要考虑后面两个一，因为后两个1查找的结果一定会包含在第一个1之中；类似的有第二层遍历时不需要考虑第三个1，以此类推这样就避免了重复元素导致的重复结果。
+     * 在树枝上重复元素则任意选取。
+     * @author xs
+     * @param candidates 有重复元素的集合（已排序）
+     * @param target
+     * @param used 已取元素标志位
+     * @param sum
+     * @param startIndex
+     * @return void
+     */
+    private void backTracing_combinationSum2(int[] candidates, int target, int[] used, int sum, int startIndex) {
+        if (sum == target) {
+            result.add(new ArrayList<>(path));
+        }
+        for (int i = startIndex; i < candidates.length && sum < target; i++) {
+            if (i > 0 && candidates[i] == candidates[i - 1] && used[i - 1] == 0) {
+                continue;
+            }
+            used[i] = 1;
+            path.add(candidates[i]);
+            backTracing_combinationSum2(candidates, target, used, sum + candidates[i], i + 1);
+            path.remove(path.size() - 1);
+            used[i] = 0;
+        }
+
+    }
+    /*131. 分割回文串
+    给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是 回文串 。返回 s 所有可能的分割方案。
+    回文串 是正着读和反着读都一样的字符串。*/
+    List<String> partition = new ArrayList<>();
+    List<List<String>> allPartition = new ArrayList<>();
+    public List<List<String>> partition(String s) {
+        backTracing_partition(s, 0);
+        return allPartition;
+    }
+
+    private void backTracing_partition(String s, int startIndex) {
+        if (startIndex >= s.length()) {
+            allPartition.add(new ArrayList<>(partition));
+        }
+        for (int i = startIndex; i < s.length(); i++) {
+            if (isPalindrome(s, startIndex, i + 1)) {
+                partition.add(s.substring(startIndex, i + 1));
+            }else continue;
+            backTracing_partition(s, i + 1);
+            partition.remove(partition.size() - 1);
+        }
+    }
+
+    private boolean isPalindrome(String s, int startIndex, int end) {
+        if (s.length() == 0 || startIndex > end) return false;
+        for (int i = startIndex,j = end - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void isPalindromeTest(){
+        System.out.println(partition("aaaa"));
+    }
+
+    /*93. 复原 IP 地址
+    有效 IP 地址 正好由四个整数（每个整数位于 0 到 255 之间组成，且不能含有前导 0），整数之间用 '.' 分隔。
+    例如："0.1.2.201" 和 "192.168.1.1" 是 有效 IP 地址，但是 "0.011.255.245"、"192.168.1.312" 和 "192.168@1.1" 是 无效 IP 地址。
+    给定一个只包含数字的字符串 s ，用以表示一个 IP 地址，返回所有可能的有效 IP 地址，这些地址可以通过在 s 中插入 '.' 来形成。
+    你 不能 重新排序或删除 s 中的任何数字。你可以按 任何 顺序返回答案。*/
+    List<String> resultIP = new ArrayList<>();
+    public List<String> restoreIpAddresses(String s) {
+        StringBuilder stringBuilder = new StringBuilder(s);
+        if (s.length() < 4 || s.length() > 12) return resultIP;
+        backTracing_restoreIpAddresses(stringBuilder, 0, 0);
+        return resultIP;
+    }
+
+    private void backTracing_restoreIpAddresses(StringBuilder s, int startIndex, int pointNum) {
+        if (pointNum == 3) {
+            if (isValid(s, startIndex, s.length() - 1)) {
+                resultIP.add(s.toString());
+            }
+
+        }
+        for (int i = startIndex; i < s.length(); i++) {
+            if (isValid(s, startIndex, i)) {
+                s.insert(i + 1, ".");
+                pointNum++;
+                backTracing_restoreIpAddresses(s, i + 2, pointNum);
+                pointNum--;
+                s.deleteCharAt(i + 1);
+            } else break;
+        }
+    }
+
+    private boolean isValid(StringBuilder s, int start, int end) {
+        if (start > end) return false;
+        if (s.charAt(start) == '0' && start != end) return false;
+        int num = 0;
+        for (int i = start; i <= end; i++) {
+            if (s.charAt(i) > '9' || s.charAt(i) < '0') { // 遇到非数字字符不合法
+                return false;
+            }
+            num = num * 10 + (s.charAt(i) - '0');
+            if (num > 255) { // 如果大于255了不合法
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*78. 子集
+    给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+    解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。*/
+    public List<List<Integer>> subsets(int[] nums) {
+        backTracing_subsets(nums, 0);
+        return result;
+    }
+
+    /**
+     * @description
+     * 可以不写终止条件，因为本来我们就要遍历整棵树。
+     * 有的同学可能担心不写终止条件会不会无限递归？
+     * 并不会，因为每次递归的下一层就是从i+1开始的。也就是说函数最终总会执行完毕。
+     * @author xs
+     * @param nums
+     * @param startIndex
+     * @return void
+     */
+    private void backTracing_subsets(int[] nums, int startIndex) {
+        result.add(new ArrayList<>(path));
+        for (int i = startIndex; i < nums.length; i++) {
+            path.add(nums[i]);
+            backTracing_subsets(nums, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    /*90. 子集 II
+    给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+    解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。*/
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        int[] usedNum = new int[nums.length];
+        Arrays.sort(nums);
+        backTracing_subsetsWithDup(nums, 0, usedNum);
+        return result;
+    }
+
+    private void backTracing_subsetsWithDup(int[] nums, int startIndex, int[] usedNum) {
+        result.add(new ArrayList<>(path));
+        for (int i = startIndex; i < nums.length; i++) {
+            if (i > 0 && usedNum[i - 1] == 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            usedNum[i] = 1;
+            path.add(nums[i]);
+            backTracing_subsetsWithDup(nums, i + 1, usedNum);
+            path.remove(path.size() - 1);
+            usedNum[i] = 0;
+        }
+    }
+
+    @Test
+    public void subsetsWithDupTest(){
+        System.out.println(subsetsWithDup(new int[]{1, 2, 2}));
+    }
 }
